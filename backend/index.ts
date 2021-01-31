3b44bf9ae8bc36de669e2c39bc47c5f5315fdaa4
wwalpha <wwalpha@gmail.com>
2021-01-31 06:19:01 +0000

    update

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express();
 
 // health check
-app.get('/api', (_, res) => res.send('Hello world'));
+app.get('/api', (_, res) => res.send('Hello world1'));
+
+app.get('/api/test', (_, res) => res.send('Hello world2'));
 
 app.listen(8080, () => console.log('started at port 8080'));
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -18,8 +18,19 @@ resource "aws_iam_role_policy_attachment" "ecs_task_exec" {
 }
 
 # ----------------------------------------------------------------------------------------------
+# AWS IAM Policy - Inline
+# ----------------------------------------------------------------------------------------------
+resource "aws_iam_policy" "inline" {
+  name        = "inline-policy"
+  description = "inline-policy"
+
+  policy = file("iam/inline-policy.json")
+}
+
+# ----------------------------------------------------------------------------------------------
 # AWS ECS Task Execution Policy
 # ----------------------------------------------------------------------------------------------
-data "aws_iam_role" "service_role_for_ecs" {
-  name = "AWSServiceRoleForECS"
+resource "aws_iam_role_policy_attachment" "inline" {
+  role       = aws_iam_role.ecs_task_exec.name
+  policy_arn = aws_iam_policy.inline.arn
 }
